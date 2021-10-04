b0558872a327c6d832308b6b93207a15855a3ee3
Lucio Negrello <lucionegrello@yahoo.com.ar>
2021-10-04 14:45:02 -0300

    Create Cart Context

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,7 +1,12 @@
-import {createContext} from "react";
+import {createContext} from 'react';
 
 const context = createContext({
   products: [],
+  cart: [],
+  addProduct: product => {},
+  deleteProduct: productId => {},
+  cartPrice: 0,
+  updateCartPrice: () => {},
 });
 
 export default context;
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -6,6 +6,31 @@ import apiFetch from '../resources/apiFetch';
 
 const GlobalState = props => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [cartPrice, setCartPrice] = useState(0);
+
+  const addProduct = product => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
+  const deleteProduct = productId => {
+    const position = cart.findIndex(product => product.id === productId);
+
+    if (position >= o) {
+      const newCart = [...cart];
+      newCart.splice(position, 1);
+      setCart(newCart);
+    }
+  };
+
+  const updateCartPrice = () => {
+    let newCartPrice = 0;
+    cart.map(
+      product => (newCartPrice = newCartPrice + parseFloat(product.price)),
+    );
+    setCartPrice(newCartPrice);
+  };
 
   const fetchProducts = async () => {
     const newProducts = await apiFetch.get(
@@ -19,10 +44,19 @@ const GlobalState = props => {
     fetchProducts();
   }, []);
 
+  useEffect(() => {
+    updateCartPrice();
+  }, [cart]);
+
   return (
     <Context.Provider
       value={{
         products,
+        cart,
+        setCart,
+        addProduct,
+        deleteProduct,
+        cartPrice,
       }}>
       {props.children}
     </Context.Provider>
